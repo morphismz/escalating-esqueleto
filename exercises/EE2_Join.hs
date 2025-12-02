e3e4b1a10af147fab7754f4de0e7cb5d4be2af00
Raymond Baker <baker@mercury.com>
2025-12-01 18:59:24 -0700

    WIP: EE2

--- exercises/EE2_Join.hs
+++ exercises/EE2_Join.hs
@@ -1,9 +1,11 @@
 {- HLINT ignore "Use camelCase" -}
 module EE2_Join where
 
+import Control.Arrow
 import Data.Coerce (coerce)
 import Data.Text (Text)
 import Database.Esqueleto.Experimental
+import Database.Esqueleto.PostgreSQL
 import Schema
 import Types
 
@@ -13,8 +15,15 @@ What are all our customers' favorite flavors?
 If they don't have one, give back a `Nothing`.
 -}
 a_favoriteFlavors :: DB [(Entity Customer, Maybe (Entity Flavor))]
-a_favoriteFlavors = _
-
+a_favoriteFlavors = do
+  custflavors <- select $ do
+    (customer :& mflavor) <-
+      from $ table @Customer
+        `leftJoin` table @Flavor
+         `on` \(customer :& mflavor) ->
+          (customer ^. CustomerFavoriteFlavor) ==. (mflavor ?. FlavorId)
+    pure (customer, mflavor)
+  pure custflavors
 {-
 We'd like to determine the popularity of each flavor.
 
@@ -30,7 +39,16 @@ Sample results:
 ]
 -}
 b_flavorPopularity :: DB [(Text, Int)]
-b_flavorPopularity = _
+b_flavorPopularity = do
+  t <- select $ do
+    (flavor :& customer) <- from $ table @Flavor
+      `innerJoin` table @Customer
+      `on` \(flavor :& customer) ->
+      (just $ flavor ^. FlavorId) ==. (customer ^. CustomerFavoriteFlavor)
+    groupBy (flavor ^. FlavorName)
+    orderBy [desc (countDistinct @Int (customer ^. CustomerId)), asc (flavor ^. FlavorName)]
+    pure (flavor ^. FlavorName, countDistinct (customer ^. CustomerId))
+  pure $ fmap (unValue *** unValue) t
 
 {-
 We have a concept of "groups" provided by CustomerLink and CustomerGroupParent.
@@ -38,11 +56,40 @@ We have a concept of "groups" provided by CustomerLink and CustomerGroupParent.
 Who are all the customers in the largest group?
 -}
 c_largestGroup :: DB [Entity Customer]
-c_largestGroup = _
-
+c_largestGroup = do
+  select $ do
+    ((clinkParentId, cnt) :& customerLink :& customer) <- from $
+      (do
+        custLink <- from $ table @CustomerLink
+        groupBy (custLink ^. CustomerLinkParentId)
+        orderBy [desc $ countDistinct @Int (custLink ^. CustomerLinkCustomerId)]
+        limit 1                                                                         
+        pure $ (custLink ^. CustomerLinkParentId, countDistinct @Int (custLink ^. CustomerLinkCustomerId)))
+      `innerJoin` table @CustomerLink
+      `on` (\((clinkparentId, _) :& customerLink) ->
+        clinkparentId ==. (customerLink ^. CustomerLinkParentId))
+      `innerJoin` table @Customer                                                                
+      `on` (\((_ :& customerLink) :& customer) ->
+        (customerLink ^. CustomerLinkCustomerId) ==. (customer ^. CustomerId))
+    pure $ customer
+{-  select $ do
+    custlink <- from $ table @CustomerLink
+    undefined-}
+  
 {-
 For each CustomerGroupParent with at least one customer in it, list its name,
 as well as the names of all the customers in that group.
 -}
 d_customerGroups :: DB [(Text, [Text])]
-d_customerGroups  = _
+d_customerGroups = do
+  fmap (fmap (unValue *** unValue)) $ select $ do
+    (customerGroupParent :& customerLink :& customer) <- from $ table @CustomerGroupParent
+      `innerJoin` table @CustomerLink
+      `on` (\(customerGroupParent :& customerLink) ->
+        (customerGroupParent ^. CustomerGroupParentId) ==. (customerLink ^. CustomerLinkParentId))
+      `innerJoin` table @Customer
+      `on` (\((_ :& customerLink) :& customer) ->
+        (customerLink.customerId) ==. (customer.id))
+    groupBy (customerGroupParent.id, customerGroupParent.name)
+    pure (customerGroupParent.name , maybeArray (arrayAgg (customer.name)))
+
